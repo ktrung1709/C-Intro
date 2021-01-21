db2368db2638f9fac58746cb5d77f8a6cb038571
ktrung1709 <75299342+ktrung1709@users.noreply.github.com>
2021-01-21 21:16:21 +0700

    Create ex8.c

---
+++ ex8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+  int i,j;
+
+  printf("       I    J\n");
+
+  for (i=1; i<4; i++)
+    {
+      printf("Outer %6d\n",i);
+      for(j =0; j<i; j++)
+	{
+	  printf(" Inner%9d\n",j);
+	}
+    }
+  return (0);
+}
